db7de94cd31fbf478cb7f859713e5216dfb32680
tblane <tblane88@gmail.com>
2018-10-27 23:54:15 -0400

    Added more funtionality to the app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,14 @@
 
 var friendsArray = [
     {
-        name: "Thor",
-        img: "https://media.npr.org/assets/img/2017/10/27/tra2016_comp_v005_008088.1111_wide-bc335cede10d086ba948db2d66f0093d36f0cc1e-s800-c85.jpg"
+        name: "Donald Blake",
+        img: "https://media.npr.org/assets/img/2017/10/27/tra2016_comp_v005_008088.1111_wide-bc335cede10d086ba948db2d66f0093d36f0cc1e-s800-c85.jpg",
+        answerArray: [3, 4, 5, 2, 4, 3, 5, 3 , 5, 3]
     },
     {
-        name: "Iron Man",
-        img: "https://static1.squarespace.com/static/583c906ebe659429d1106265/t/5935941859cc687293cd3f6b/1496683572683/"
+        name: "Tony Stark",
+        img: "https://static1.squarespace.com/static/583c906ebe659429d1106265/t/5935941859cc687293cd3f6b/1496683572683/",
+        answerArray: [5, 4, 1, 4, 3, 2, 5, 3, 4, 3]
     }
 ];
 
