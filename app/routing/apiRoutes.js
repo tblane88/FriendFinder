44cc2cef22d10a28b799d2fa2ba08ebf19b15ed6
tblane <tblane88@gmail.com>
2018-10-28 19:32:59 -0400

    final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,45 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+        var newFriend = req.body;
+
+        console.log("inside the function");
+        console.log(newFriend);
+        
+    
+            
+        var comparison = [];
+        var closeFriend = "";
+        
+        for (var i = 0; i < friendsData.length; i++) {
+            var total = 0;
+    
+            for (var h = 0; h < friendsData[i].answerArray.length; h++) {
+    
+    
+                total += Math.abs(parseInt(friendsData[i].answerArray[h]) - parseInt(newFriend.answerArray[h]));
+            };
+            comparison.push(total)
+        };
+        
+    
+            var lowestNum = Math.min.apply(null, comparison);
+    
+            var location = comparison.indexOf(lowestNum);
+    
+            closeFriend = friendsData[location];
+            console.log(closeFriend);
+            
+            friendsData.push(newFriend);
+
+            res.json(closeFriend);
+      
+        
+
+
     });
+
+
+  
 
 };
